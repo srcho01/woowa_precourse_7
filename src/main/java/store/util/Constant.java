8a71a510ab525879c3b4d8e46e536e2bb733cc09
srcho01 <seorim5306@gmail.com>
2024-11-10 02:24:07 +0900

    feat: 에러 코드 enum 정의

---
+++ src/main/java/store/util/Constant.java
@@ -0,0 +1,7 @@
+package store.util;
+
+public class Constant {
+
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
+}
---
+++ src/main/java/store/util/ErrorCode.java
@@ -0,0 +1,25 @@
+package store.util;
+
+public enum ErrorCode {
+
+    INVALID_INPUT_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    PRODUCT_NOT_FOUND("존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    QUANTITY_EXCEED_STOCK("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    INVALID_INPUT("잘못된 입력입니다. 다시 입력해 주세요.");
+
+
+    private final String message;
+
+    ErrorCode(String message) {
+        this.message = Constant.ERROR_PREFIX + message;
+    }
+
+    public IllegalArgumentException exception() {
+        return new IllegalArgumentException(this.message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
